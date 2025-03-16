fcf5ac478a0882dd1ce7e325563ff5cd21a57642
Shivam <shivamtamboli66@gmail.com>
2025-03-16 15:37:28 +0530

    create a function and map with ulrs when we will fire this urls then these functions will run and display to the client

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
 from datetime import datetime
 
-def test(equest):
+def test(request):
     date = datetime.now()
     print("test function is called from view")
     return HttpResponse("<h1>Hello this is index page</h1>" + str(date)) 
+
+def about(request):
+    return HttpResponse("<h1>This is about page</h1>")
+
+def part(request):
+    print("This is from part function")
+    return HttpResponse("<h1>This is from part side</h1>")
