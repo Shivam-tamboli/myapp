6d67c09408ab3f0992f97a58a539aaa9d7e23f8a
Shivam <shivamtamboli66@gmail.com>
2025-03-20 22:04:32 +0530

    send data dynamically to the client

--- myapp/views.py
+++ myapp/views.py
@@ -4,9 +4,32 @@ from django.shortcuts import render
 
 def home(request):
     date = datetime.now()
+    # sending data dynamically to the client.
+    isActive=True
+    name="Shivam Tamboli"
+    list_of_program=[
+        'WAP to print even and odd numbers',
+        'WAP to print prime numbers'
+    ]
+    
+    Student={
+        'Student_name':'Satyam',
+        'Student_college':'xyz',
+        'Student_city':'Indore'
+    }
+    
+    # how to share this data on the template.
+    data={
+        "date":date,
+        "isActive":isActive,
+        "name":name,
+        "list_of_program":list_of_program,
+        "Student":Student
+        
+    }
     print("test function is called from view")
     # return HttpResponse("<h1>Hello this is index page</h1>" + str(date)) 
-    return render(request,"home.html",{})
+    return render(request,"home.html",data)#we will use third paramater of render to share the data with the template.
 
 def about(request):
     # return HttpResponse("<h1>This is about page</h1>")
