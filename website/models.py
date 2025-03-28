036b3f564c9cee0666e0285d5e4d4b7b48c2ddcc
Shivam <shivamtamboli66@gmail.com>
2025-03-28 21:35:54 +0530

    create a model it will intreact with the Database

--- website/models.py
+++ website/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model): #in parameter passed parent class so it can use the features of parent class.
+    name=models.CharField(max_length=200)
+    college=models.CharField(max_length=200)
+    age=models.IntegerField(max_length=100)
+    is_active=models.BooleanField(default=False)
