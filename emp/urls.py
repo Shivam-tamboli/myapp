2facbd5baaea118c5b6a3dea0da84ca2d8e08013
Shivam <shivamtamboli66@gmail.com>
2025-04-01 21:32:45 +0530

    map urls

--- emp/urls.py
+++ emp/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+#from django.contrib import admin
 from django.urls import path
-from .views import  home ,about,part,services
+from .views import   student_home
 
 
 urlpatterns = [
-    path("home/",student_home)
+    path("home/", student_home)
 ]
