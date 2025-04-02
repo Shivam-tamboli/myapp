79969dc7fbab77f7bce412fcc6e5faca89bcfc7c
Shivam <shivamtamboli66@gmail.com>
2025-04-02 19:50:36 +0530

    update method name

--- emp/views.py
+++ emp/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def student_home(request):
+def emp_home(request):
     return HttpResponse("student home page")
