163b5929a20774f439953aa2cc2ff8c85cf02cf9
Shivam <shivamtamboli66@gmail.com>
2025-04-02 20:08:57 +0530

    render data to the client

--- emp/views.py
+++ emp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def emp_home(request):
-    return HttpResponse("student home page")
+    return render(request, "emp/home.html",{})
