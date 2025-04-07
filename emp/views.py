9047d3c728dd95aec2499b1f1cfaff114ecc00a2
Shivam <shivamtamboli66@gmail.com>
2025-04-07 20:21:37 +0530

    check data recive or not, if it recives it's redirect to the home page

--- emp/views.py
+++ emp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
@@ -6,4 +6,8 @@ def emp_home(request):
     return render(request, "emp/home.html",{})
 
 def add_emp(request):
+    if request.method == "POST":
+        print("Reciving data")
+        return redirect("/emp/home/")
+        
     return render(request, "emp/add_emp.html",{})
