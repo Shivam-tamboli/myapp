0ea71a537462651cbc327e6eb35a24ceeef6aa2e
Shivam <shivamtamboli66@gmail.com>
2025-04-07 22:08:05 +0530

    make migrations and update the model

--- emp/views.py
+++ emp/views.py
@@ -18,6 +18,30 @@ def add_emp(request):
         emp_departent=request.POST.get(emp_departent)
         
         
+        #create models object to set the data
+        
+        #create constructor
+        e=Emp()
+        
+        e.name=emp_name
+        e.id=emp_id
+        e.phone=emp_phone
+        e.address=emp_address
+       # e.working=emp_working
+        e.department=emp_departent
+        #if nothing is filled it will be false.
+        if emp_working is None:
+            e.working=False
+            #if something is filled then true.
+        else:
+            e.working=True
+        
+        # -------object is ready.---------
+        
+        
+        #save the object.
+        e.save()
+        
         return redirect("/emp/home/")
         
     return render(request, "emp/add_emp.html",{})
