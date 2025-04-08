49618941b36903299d911396beca5c1e6d8eeda5
Shivam <shivamtamboli66@gmail.com>
2025-04-08 20:30:45 +0530

    map url for update employee

--- emp/urls.py
+++ emp/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 #from django.contrib import admin
 from django.urls import path
-from .views import emp_home , add_emp , delete_emp
+from .views import emp_home , add_emp , delete_emp, update_emp,
 
 
 urlpatterns = [
     path("home/", emp_home),
     path("add-emp/", add_emp),
     path("delete-emp/<int:emp_id>/", delete_emp),
+    path("update-emp/<int:emp_id>/", update_emp),
 ]
