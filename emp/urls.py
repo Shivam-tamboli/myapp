f0b4e882698e85a6a919fd9d23e8a5764c57ad73
Shivam <shivamtamboli66@gmail.com>
2025-04-10 19:10:11 +0530

    map with the urls

--- emp/urls.py
+++ emp/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 #from django.contrib import admin
 from django.urls import path
-from .views import emp_home , add_emp , delete_emp, update_emp
+from .views import emp_home , add_emp , delete_emp, update_emp, do_update_emp
 
 
 urlpatterns = [
@@ -24,4 +24,5 @@ urlpatterns = [
     path("add-emp/", add_emp),
     path("delete-emp/<int:emp_id>/", delete_emp),
     path("update-emp/<int:emp_id>/", update_emp),
+    path("do-update-emp/<int:emp_id>/", do_update_emp),
 ]
